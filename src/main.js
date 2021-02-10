22dba5fae6100a198f719d99c3c132dbd46108d7
Levi Kohler <kohler.la01@gmail.com>
2021-02-09 16:26:25 -0800

    add weird search and find trend buttons as well as troubleshoot
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,19 +5,21 @@ import './css/styles.css';
 
 function clearFields() {
   $("#results0").empty();
+  $('#weirdresult').empty();
   $("#result").empty();
   $("#randomresult").empty();
   $("#giphy").val("");
+  $("#weirdgiphy").val("");
+  $("#trends").empty();
 }
 
 $(document).ready(function () {
-
   $("#findgiphy").click(function () {
     const search = $("#giphy").val();
-
+    const apiURL = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_KEY}&limit=5&rating=pg-13`;
+    
     let promise = new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      const apiURL = `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${process.env.API_KEY}&limit=5&rating=pg-13`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
@@ -28,7 +30,7 @@ $(document).ready(function () {
       request.open("GET", apiURL, true);
       request.send();
     });
-
+    
     promise.then(function (response) {
       clearFields();
       const body = JSON.parse(response);
@@ -39,14 +41,38 @@ $(document).ready(function () {
       $('#results0').text(`There was an error processing your request: ${error}`);
     });
   });
-  //
-  $("#findtrendgiphy").click(function () {
-    console.log("SUCCESS");
 
+  $("#findweirdgiphy").click(function () {
+    const search = $("#weirdgiphy").val();
+    const apiURL = `http://api.giphy.com/v1/gifs/translate?s=${search}&api_key=${process.env.API_KEY}&weirdness=10`;
+    
+    let promise = new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", apiURL, true);
+      request.send();
+    });
+    
+    promise.then(function (response) {
+      clearFields();
+      const body = JSON.parse(response);
+        $("#weirdresult").append(`<img src='${body.data.images.original.url}' alt="result gif">`);
+    }, function (error) {
+      $('#weirdresult').text(`There was an error processing your request: ${error}`);
+    });
+  });
+  
+  $("#findtrendgiphy").click(function () {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const api = `http://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=5&rating=pg-13`;
-
+      
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -57,7 +83,7 @@ $(document).ready(function () {
       request.open("GET", api, true);
       request.send();
     });
-
+    
     promise.then(function(response) {
       clearFields();
       const body = JSON.parse(response);
@@ -68,12 +94,13 @@ $(document).ready(function () {
       $('#result').text(`There was an error processing your request: ${error}`);
     });
   });
-  //
-  $('#randomgiphy').click(function () {
+  
+  $("#showtrends").click(function() {
+
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const api = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&rating=pg-13`
-
+      const api = `http://api.giphy.com/v1/trending/searches?api_key=${process.env.API_KEY}`;
+      
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -84,7 +111,34 @@ $(document).ready(function () {
       request.open("GET", api, true);
       request.send();
     });
+    
+    promise.then(function(response) {
+      clearFields();
+      const body = JSON.parse(response);
+      for (let index = 0; index < body.data.length; index++) {
+        $('#trends').append(`<li>${body.data[index]}</li>`);
+      }
+    }, function(error) {
+      $('#trends').text(`There was an error processing your request: ${error}`);
+    });
+  });
 
+  $('#randomgiphy').click(function () {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const api = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&rating=pg-13`
+      
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", api, true);
+      request.send();
+    });
+    
     promise.then(function(response) {
       clearFields();
       const body = JSON.parse(response);
